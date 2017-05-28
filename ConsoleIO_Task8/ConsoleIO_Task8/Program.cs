7ccadb67704c41938248bc15fafa2d1349166894
Simeon Laplev <thewiffedshot@gmail.com>
2017-05-28 22:59:12 +0300

    Add files via upload

---
+++ ConsoleIO_Task8/ConsoleIO_Task8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIO_Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1, v2, v3, v4, v5;
+
+            Console.WriteLine("Please enter 5 numbers.\n");
+           
+            v1 = Convert.ToInt32(Console.ReadLine());
+            v2 = Convert.ToInt32(Console.ReadLine());
+            v3 = Convert.ToInt32(Console.ReadLine());
+            v4 = Convert.ToInt32(Console.ReadLine());
+            v5 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nThe sum of the 5 numbers is {0}.", v1 + v2 + v3 + v4 + v5);
+            Console.ReadKey();
+        }
+    }
+}
