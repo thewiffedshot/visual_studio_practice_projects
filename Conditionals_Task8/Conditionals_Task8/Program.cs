8f80d5a8646408ac965a9420e3bdff38af3eb51f
Simeon Laplev <thewiffedshot@gmail.com>
2017-05-29 23:42:09 +0300

    Added one of daily tasks.

---
+++ Conditionals_Task8/Conditionals_Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals_Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double d;
+
+            Console.Write("Please input a number or string of text: ");
+            var var = Console.ReadLine();
+
+            if (Int32.TryParse(var, out n)) { Console.WriteLine("\nYour integer is: {0}", n + 1); }
+            else if (Double.TryParse(var, out d)) { Console.WriteLine("\nYour double is: {0}", d + 1); }
+            else { Console.WriteLine("\nYour string is: \"{0}\"", var + "*"); }
+
+            Console.ReadKey();
+        }
+    }
+}
