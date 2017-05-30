e9d0b62c08969fa787829c70b5e6eef4fc1b8618
Simeon Laplev <thewiffedshot@gmail.com>
2017-05-31 02:14:30 +0300

    Upload

---
+++ for_loop_test/for_loop_test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 9; a++)
+            {
+                for (int b = 0; b <= 9; b++)
+                {
+                    for (int c = 0; c <= 9; c++)
+                    {
+                        int d = a + b - c;
+
+                        if (d >= 0 && d <= 9)
+                            Console.WriteLine(a + " " + b + " " + c + " " + d);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+
+            // a + b = c + d
+            // d = a + b - c
+        }
+    }
+}
