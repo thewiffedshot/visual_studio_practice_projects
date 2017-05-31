bf79a7dd80ad48f47cb0856f7376aad265ca5d7f
Simeon Laplev <thewiffedshot@gmail.com>
2017-05-31 23:53:55 +0300

    Upload

---
+++ Loops_Task1/Loops_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter number 'n': ");
+            uint n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine(); // Cosmetic touch.
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Loops_Task2/Loops_Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter number 'n': ");
+            uint n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine(); // Cosmetic touch.
+
+            for (int i = 1; i <= n; i++)
+            {
+                if ((i % 3 != 0) && (i % 7 != 0))
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
