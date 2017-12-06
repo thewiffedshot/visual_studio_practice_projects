9efa87e8537eeb79e5d2f2ddfa4a1d2da592296f
thewiffedshot <thewiffedshot@gmail.com>
2017-12-06 22:08:03 +0200

    Quick little recursion task.

---
+++ Recursion_Exercise1/Recursion_Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 'n' as the number of integers you want summed starting from 1: ");
+            string sum = RecursiveSum(int.Parse(Console.ReadLine())).ToString();
+
+            Console.WriteLine("\n{0}", sum);
+            Console.ReadKey();
+        }
+
+        static int RecursiveSum(int integer)
+        {
+            int sum = integer;
+
+            if (integer < 1) return 0;
+            
+            sum += RecursiveSum(integer - 1);
+
+            return sum;
+        }
+    }
+}
