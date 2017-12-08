3cae5b55f2477115850cc8c885dc2ecb285bd58b
Simeon Laplev <thewiffedshot@gmail.com>
2017-12-09 00:51:55 +0200

    Factorial.

---
+++ Recursion_Exercise2/Recursion_Exercise2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Recursion_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a natural number: ");
+            string result = Factorial(int.Parse(Console.ReadLine())).ToString();
+            Console.WriteLine("The factorial is: {0}", result);
+
+            Console.ReadKey();
+        }
+
+        static BigInteger Factorial(int n)
+        {
+            if (n < 1) return 1;
+
+            BigInteger product = n;
+            product *= Factorial(n - 1);
+
+            return product;
+        }
+    }
+}
