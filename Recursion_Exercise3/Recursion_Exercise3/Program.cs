fb42bf98d3cf46061bb59b1bab604e44d7900f85
thewiffedshot <thewiffedshot@gmail.com>
2017-12-11 13:33:35 +0200

    Recursive multiplication.

---
+++ Recursion_Exercise3/Recursion_Exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(2, 1));
+
+            Console.ReadKey();
+        }
+
+        static int Multiply(int a, int b)
+        {
+            if (a == 1) return b;
+
+            int product = b + Multiply(a - 1, b);
+
+            return product;
+        }
+    }
+}
