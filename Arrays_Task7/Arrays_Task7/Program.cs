98b444c9ec828251fbdc94fdf2bfb2edd8a3a6bd
Simeon Laplev <thewiffedshot@gmail.com>
2018-02-04 20:08:18 +0200

    Upload

---
+++ Arrays_Task7/Arrays_Task7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Task7
+{
+    class Program
+    {
+        static int[] nums;
+        static int n, k;
+
+        static void Main(string[] args)
+        {
+            n = ReadInt("Въведете дължината на масива: ");
+
+            nums = new int[n];
+
+            k = ReadInt("Въведете дължината на поредицата за сумиране: ");
+
+            PopulateArray(nums);
+
+            int maxSum = int.MinValue, sum = 0, j = 0;
+
+            for (int i = 0; i < nums.Length; i++, sum = 0, j = 0)
+            {
+                while (j < k)
+                {
+                    if (i + j == nums.Length)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        sum += nums[i + j];
+                    }
+
+                    j++;
+                }
+
+                if (sum > maxSum && j == k) maxSum = sum;
+            }
+
+            Console.WriteLine("Максималната сума при поредица дълга {0} е {1}.", k, maxSum);
+
+            Console.ReadKey();
+        }
+
+        static void PopulateArray(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = ReadInt("a[" + i + "] = ");
+            }
+        }
+
+        static int ReadInt(string prompt)
+        {
+            int num;
+
+            Console.Write(prompt);
+
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.Write(prompt);
+            }
+
+            return num;
+        }
+    }
+}
---
+++ Arrays_Task8/Arrays_Task8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Task8
+{
+    class Program
+    {
+        static int[] nums = { 1, 3, 3, 7, 4, 2, 0, 6, 9 };
+
+        static void Main(string[] args)
+        {
+            SelectionSort(nums);
+            PrintArray(nums);
+
+            Console.ReadKey();
+        }
+
+        static void SelectionSort(int[] a)
+        {
+            int placeHolder;
+            int minElement = int.MaxValue;
+            int minIndex = 0;
+
+            for (int i = 0; i < a.Length - 1; i++, minElement = int.MaxValue)
+            {
+                placeHolder = a[i];
+
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] < minElement)
+                    {
+                        minElement = a[j];
+                        minIndex = j;
+                    }
+                }
+
+                a[i] = minElement;
+                a[minIndex] = placeHolder;         
+            }
+        }
+
+        static void PrintArray(int[] a)
+        {
+            foreach (int num in a)
+            {
+                Console.Write("{0} ", num);
+            }
+        }
+    }
+}
