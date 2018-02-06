3ea4444ffa4a13e54ff037c3edaff7ea634c2386
Simeon Laplev <thewiffedshot@gmail.com>
2018-02-06 15:59:09 +0200

    Upload.
    Finished this task.

--- Arrays_Task12/Arrays_Task12/Program.cs
+++ Arrays_Task12/Arrays_Task12/Program.cs
@@ -20,25 +20,48 @@ namespace Arrays_Task12
             matrix3 = new int[n, n];
             matrix4 = new int[n, n];
 
-            //try
-            //{
+            try
+            {
                 PopulateMatrix(matrix1, 1);
                 PopulateMatrix(matrix2, 2);
                 PopulateMatrix(matrix3, 3);
                 PopulateMatrix(matrix4, 4);
 
-                //PrintMatrix(matrix1); // TODO...
-                //PrintMatrix(matrix2);
-                //PrintMatrix(matrix3);
-                //PrintMatrix(matrix4);
+                Console.WriteLine();
 
-            //} catch (Exception e)
-            //{
-                //Console.WriteLine(e.Message);
-                //Console.ReadKey();
-            //}
+                PrintMatrix(matrix1);
+                PrintMatrix(matrix2);
+                PrintMatrix(matrix3);
+                PrintMatrix(matrix4);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
 
             Console.ReadKey();
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            int maxLength = (n * n).ToString().Length;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int intervalLength = maxLength - matrix[i, j].ToString().Length;
+                    string interval = " ";
+
+                    for (int o = 0; o < intervalLength; o++) interval += " ";
+
+                    Console.Write(matrix[i, j].ToString() + interval);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n");
         }
 
         static void PopulateMatrix(int[,] matrix, int type)
@@ -112,9 +135,110 @@ namespace Arrays_Task12
 
                     break;
                 case 4:
+                    counter = 1;
+                    int leftBorder = 0, rightBorder = n - 1, topBorder = 0, botBorder = n - 1;
 
-                    //TODO...
+                    bool horizontal = false;
+                    bool right = true;
+                    bool _down = true;
 
+                    while (counter <= n * n)
+                    {
+                        if (horizontal)
+                        {
+                            if (right)
+                            {
+                                for (int i = leftBorder; i <= rightBorder; i++)
+                                {
+                                    if (_down)
+                                    {
+                                        matrix[topBorder, i] = counter;
+
+                                        counter++;
+                                    }
+                                    else
+                                    {
+                                        matrix[botBorder, i] = counter;
+
+                                        counter++;
+                                    }
+                                }
+
+                                botBorder--;
+                                right = !right;
+                            }
+                            else
+                            {
+                                for (int i = rightBorder; i >= leftBorder; i--)
+                                {
+                                    if (_down)
+                                    {
+                                        matrix[topBorder, i] = counter;
+
+                                        counter++;
+                                    }
+                                    else
+                                    {
+                                        matrix[botBorder, i] = counter;
+
+                                        counter++;
+                                    }
+                                }
+
+                                topBorder++;
+                                right = !right;
+                            }
+
+                            horizontal = !horizontal;
+                        }
+                        else
+                        {
+                            if (_down)
+                            {
+                                for (int i = topBorder; i <= botBorder; i++)
+                                {
+                                    if (right)
+                                    {
+                                        matrix[i, leftBorder] = counter;                  
+
+                                        counter++;
+                                    }
+                                    else
+                                    {
+                                        matrix[i, rightBorder] = counter;
+
+                                        counter++;
+                                    }
+                                }
+
+                                leftBorder++;
+                                _down = !_down;
+                            }
+                            else
+                            {
+                                for (int i = botBorder; i >= topBorder; i--)
+                                {
+                                    if (right)
+                                    {
+                                        matrix[i, leftBorder] = counter;
+
+                                        counter++;
+                                    }
+                                    else
+                                    {
+                                        matrix[i, rightBorder] = counter;
+
+                                        counter++;
+                                    }
+                                }
+
+                                rightBorder--;
+                                _down = !_down;
+                            }
+
+                            horizontal = !horizontal;
+                        }
+                    }
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
