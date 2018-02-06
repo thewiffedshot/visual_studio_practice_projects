17b0daa832d26cc147ef54abb855fe844147163f
Simeon Laplev <sovietdoggo@station.Lambda>
2018-02-06 20:55:28 +0200

    TODO...

---
+++ Arrays_Task13/Arrays_Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Task12
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+
+			Console.ReadKey ();
+		}
+	}
+}
