ffab794ddebd032d615e8bca3cba3972f892dba3
thewiffedshot <thewiffedshot@gmail.com>
2018-02-13 00:01:57 +0200

    Upload.

--- Arrays_Task15/Arrays_Task15/Program.cs
+++ Arrays_Task15/Arrays_Task15/Program.cs
@@ -4,11 +4,30 @@ namespace Arrays_Task15
 {
     class MainClass
     {
+        static char[] alphabet = new char[26];
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PopulateArray(alphabet);
+
+            Console.WriteLine("Please input a word: ");
+            string str = Console.ReadLine();
+
+            foreach (char c in str)
+            {
+                Console.Write("{0} ", (int)(c - 'a'));
+            }
+
 
             Console.ReadKey();
+        }
+
+        static void PopulateArray(char[] a)
+        {
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                alphabet[i - 'a'] = Convert.ToChar(i);
+            }
         }
     }
 }
---
+++ Arrays_Task16/Arrays_Task16/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Arrays_Task16
+{
+    class MainClass
+    {
+        static int[] array = { 1, 5, 5, 7, 9, 10, 11, 15, 35, 35, 39, 42 };
+
+        public static void Main(string[] args)
+        {
+            int index = BinarySearch(array, 39);
+
+            Console.ReadKey();
+        }
+
+        static int BinarySearch(int[] _array, int a)
+        {
+            int start = 0, end = _array.Length - 1, mid, n = 0;
+
+            while (start < end && n < _array.Length / 2)
+            {
+                mid = start + (end - start) / 2;
+
+                if (_array[mid] == a)
+                    return mid;
+                
+                if (a < _array[mid])
+                {
+                    end = mid;
+                }
+                else if (a > _array[mid])
+                {
+                    start = mid;
+                }
+
+                if (a > _array[mid] && a == _array[end])
+                    return end;
+
+                n++;
+            }
+
+            if (_array[start] == a) return start;
+
+            return int.MinValue;
+        }
+    }
+}
