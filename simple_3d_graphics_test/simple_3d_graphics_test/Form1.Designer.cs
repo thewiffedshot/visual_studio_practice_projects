0b260cbfa9f2d15fefd3141cf717891347ec2ec4
thewiffedshot <thewiffedshot@gmail.com>
2018-03-21 23:06:56 +0200

    upload
    Rotating via matricies seems to cause a problem with floating point precision and vector shrinking. TODO: Implement rotations via Quaternions.

--- simple_3d_graphics_test/simple_3d_graphics_test/Form1.Designer.cs
+++ simple_3d_graphics_test/simple_3d_graphics_test/Form1.Designer.cs
@@ -42,11 +42,25 @@
             this.trackDistance = new System.Windows.Forms.TrackBar();
             this.labelDistance = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelRotZ = new System.Windows.Forms.Label();
+            this.labelRotY = new System.Windows.Forms.Label();
+            this.labelRotX = new System.Windows.Forms.Label();
+            this.trackRotZ = new System.Windows.Forms.TrackBar();
+            this.trackRotY = new System.Windows.Forms.TrackBar();
+            this.trackRotX = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackX)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackY)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackZ)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackDistance)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotX)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -70,7 +84,7 @@
             // 
             // trackX
             // 
-            this.trackX.Location = new System.Drawing.Point(647, 12);
+            this.trackX.Location = new System.Drawing.Point(647, 48);
             this.trackX.Maximum = 50;
             this.trackX.Minimum = -50;
             this.trackX.Name = "trackX";
@@ -80,7 +94,7 @@
             // 
             // trackY
             // 
-            this.trackY.Location = new System.Drawing.Point(647, 63);
+            this.trackY.Location = new System.Drawing.Point(647, 99);
             this.trackY.Maximum = 50;
             this.trackY.Minimum = -50;
             this.trackY.Name = "trackY";
@@ -90,7 +104,7 @@
             // 
             // trackZ
             // 
-            this.trackZ.Location = new System.Drawing.Point(647, 114);
+            this.trackZ.Location = new System.Drawing.Point(647, 150);
             this.trackZ.Maximum = 50;
             this.trackZ.Minimum = -50;
             this.trackZ.Name = "trackZ";
@@ -102,7 +116,7 @@
             // 
             this.labelX.AutoSize = true;
             this.labelX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelX.Location = new System.Drawing.Point(901, 12);
+            this.labelX.Location = new System.Drawing.Point(901, 48);
             this.labelX.Name = "labelX";
             this.labelX.Size = new System.Drawing.Size(21, 24);
             this.labelX.TabIndex = 5;
@@ -112,7 +126,7 @@
             // 
             this.labelY.AutoSize = true;
             this.labelY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelY.Location = new System.Drawing.Point(901, 63);
+            this.labelY.Location = new System.Drawing.Point(901, 99);
             this.labelY.Name = "labelY";
             this.labelY.Size = new System.Drawing.Size(21, 24);
             this.labelY.TabIndex = 6;
@@ -122,7 +136,7 @@
             // 
             this.labelZ.AutoSize = true;
             this.labelZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelZ.Location = new System.Drawing.Point(901, 114);
+            this.labelZ.Location = new System.Drawing.Point(901, 150);
             this.labelZ.Name = "labelZ";
             this.labelZ.Size = new System.Drawing.Size(21, 24);
             this.labelZ.TabIndex = 7;
@@ -132,7 +146,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(902, 36);
+            this.label1.Location = new System.Drawing.Point(902, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(17, 16);
             this.label1.TabIndex = 8;
@@ -142,7 +156,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(902, 87);
+            this.label2.Location = new System.Drawing.Point(902, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(18, 16);
             this.label2.TabIndex = 9;
@@ -152,7 +166,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(902, 138);
+            this.label3.Location = new System.Drawing.Point(902, 174);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(17, 16);
             this.label3.TabIndex = 10;
@@ -160,7 +174,7 @@
             // 
             // trackDistance
             // 
-            this.trackDistance.Location = new System.Drawing.Point(647, 210);
+            this.trackDistance.Location = new System.Drawing.Point(647, 643);
             this.trackDistance.Maximum = 1000;
             this.trackDistance.Name = "trackDistance";
             this.trackDistance.Size = new System.Drawing.Size(257, 45);
@@ -171,7 +185,7 @@
             // 
             this.labelDistance.AutoSize = true;
             this.labelDistance.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDistance.Location = new System.Drawing.Point(901, 210);
+            this.labelDistance.Location = new System.Drawing.Point(901, 643);
             this.labelDistance.Name = "labelDistance";
             this.labelDistance.Size = new System.Drawing.Size(21, 24);
             this.labelDistance.TabIndex = 12;
@@ -181,17 +195,138 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(676, 187);
+            this.label4.Location = new System.Drawing.Point(676, 620);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(209, 20);
             this.label4.TabIndex = 13;
             this.label4.Text = "Distance from view plane";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(651, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 31);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Translation Vector";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(651, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(293, 31);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Rotation Vector (deg)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(902, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Z";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(902, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 16);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Y";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(902, 269);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "X";
+            // 
+            // labelRotZ
+            // 
+            this.labelRotZ.AutoSize = true;
+            this.labelRotZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRotZ.Location = new System.Drawing.Point(901, 347);
+            this.labelRotZ.Name = "labelRotZ";
+            this.labelRotZ.Size = new System.Drawing.Size(21, 24);
+            this.labelRotZ.TabIndex = 21;
+            this.labelRotZ.Text = "0";
+            // 
+            // labelRotY
+            // 
+            this.labelRotY.AutoSize = true;
+            this.labelRotY.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRotY.Location = new System.Drawing.Point(901, 296);
+            this.labelRotY.Name = "labelRotY";
+            this.labelRotY.Size = new System.Drawing.Size(21, 24);
+            this.labelRotY.TabIndex = 20;
+            this.labelRotY.Text = "0";
+            // 
+            // labelRotX
+            // 
+            this.labelRotX.AutoSize = true;
+            this.labelRotX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRotX.Location = new System.Drawing.Point(901, 245);
+            this.labelRotX.Name = "labelRotX";
+            this.labelRotX.Size = new System.Drawing.Size(21, 24);
+            this.labelRotX.TabIndex = 19;
+            this.labelRotX.Text = "0";
+            // 
+            // trackRotZ
+            // 
+            this.trackRotZ.Location = new System.Drawing.Point(647, 347);
+            this.trackRotZ.Maximum = 90;
+            this.trackRotZ.Minimum = -90;
+            this.trackRotZ.Name = "trackRotZ";
+            this.trackRotZ.Size = new System.Drawing.Size(257, 45);
+            this.trackRotZ.TabIndex = 18;
+            this.trackRotZ.Scroll += new System.EventHandler(this.trackRotZ_Scroll);
+            // 
+            // trackRotY
+            // 
+            this.trackRotY.Location = new System.Drawing.Point(647, 296);
+            this.trackRotY.Maximum = 90;
+            this.trackRotY.Minimum = -90;
+            this.trackRotY.Name = "trackRotY";
+            this.trackRotY.Size = new System.Drawing.Size(257, 45);
+            this.trackRotY.TabIndex = 17;
+            this.trackRotY.Scroll += new System.EventHandler(this.trackRotY_Scroll);
+            // 
+            // trackRotX
+            // 
+            this.trackRotX.Location = new System.Drawing.Point(647, 245);
+            this.trackRotX.Maximum = 90;
+            this.trackRotX.Minimum = -90;
+            this.trackRotX.Name = "trackRotX";
+            this.trackRotX.Size = new System.Drawing.Size(257, 45);
+            this.trackRotX.TabIndex = 16;
+            this.trackRotX.Scroll += new System.EventHandler(this.trackRotX_Scroll);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(951, 700);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.labelRotZ);
+            this.Controls.Add(this.labelRotY);
+            this.Controls.Add(this.labelRotX);
+            this.Controls.Add(this.trackRotZ);
+            this.Controls.Add(this.trackRotY);
+            this.Controls.Add(this.trackRotX);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.labelDistance);
             this.Controls.Add(this.trackDistance);
@@ -216,6 +351,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.trackY)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackZ)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackDistance)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackRotX)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -237,6 +375,17 @@
         private System.Windows.Forms.TrackBar trackDistance;
         private System.Windows.Forms.Label labelDistance;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelRotZ;
+        private System.Windows.Forms.Label labelRotY;
+        private System.Windows.Forms.Label labelRotX;
+        private System.Windows.Forms.TrackBar trackRotZ;
+        private System.Windows.Forms.TrackBar trackRotY;
+        private System.Windows.Forms.TrackBar trackRotX;
     }
 }
 
--- simple_3d_graphics_test/simple_3d_graphics_test/Form1.cs
+++ simple_3d_graphics_test/simple_3d_graphics_test/Form1.cs
@@ -28,7 +28,7 @@ namespace simple_3d_graphics_test
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Translate(cubeOne, new Vector3(-200, -200, 50));
+            //Translate(cubeOne, new Vector3(-200, -200, 50));
             labelDistance.Text = trackDistance.Value.ToString();
             //Translate(cubeOne, new Vector3(-viewPoint.X * 10, -viewPoint.Y * 10, -viewPoint.Z * 10));
         }
@@ -40,6 +40,89 @@ namespace simple_3d_graphics_test
                 vertices[i].position.X += t.X;
                 vertices[i].position.Y += t.Y;
                 vertices[i].position.Z += t.Z;
+            }
+        }
+
+        private Vector4 GetCenter(Vertex[] mesh)
+        {
+            Vector4 average = new Vector4(0, 0, 0, 1);
+            int counter = 0;
+
+            /*foreach (Vertex vertex in mesh)
+            {
+                average += vertex.position;
+                counter++;
+            }*/
+
+            average += mesh[0].position;
+            average += mesh[7].position;
+
+
+            /*if (counter != 0)
+            average /= counter;*/
+
+            average /= 2;
+            average.W = 1;
+
+            return average;
+        }
+
+        private void RotateZ(Vertex[] vertices, float angle)
+        {
+            Vector4 center = GetCenter(vertices);
+            float tX = center.X, tY = center.Y, tZ = center.Z;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // Move to origin
+                vertices[i].position.X -= tX;
+                vertices[i].position.Y -= tY;
+
+                // Rotate at origin...
+                vertices[i].position.X = vertices[i].position.X * (float)Math.Cos(angle) + vertices[i].position.Y * (float)Math.Sin(angle);
+                vertices[i].position.Y = -vertices[i].position.X * (float)Math.Sin(angle) + vertices[i].position.Y * (float)Math.Cos(angle);       
+
+                // Translate back vertex...
+                vertices[i].position.X += tX;
+                vertices[i].position.Y += tY;
+            }
+        }
+
+        private void RotateY(Vertex[] vertices, float angle)
+        {
+            Vector4 center = GetCenter(vertices);
+            float tX = center.X, tY = center.Y, tZ = center.Z;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // Rotate at origin...
+                vertices[i].position.X = tX * (float)Math.Cos(angle) - tZ * (float)Math.Sin(angle) - tX;
+                vertices[i].position.Y = 0;
+                vertices[i].position.Z = tX * (float)Math.Sin(angle) + tZ * (float)Math.Cos(angle) - tZ;
+
+                // Translate back vertex...
+                vertices[i].position.X += tX;
+                vertices[i].position.Y += tY;
+                vertices[i].position.Z += tZ;
+            }
+        }
+
+        private void RotateX(Vertex[] vertices, float angle)
+        {
+            Vector4 center = GetCenter(vertices);
+            float tX = center.X, tY = center.Y, tZ = center.Z;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // Rotate at origin...
+                vertices[i].position.X = 0;
+                vertices[i].position.Y = tY * (float)Math.Cos(angle) - tZ * (float)Math.Sin(angle) - tY;
+                vertices[i].position.Z = tY * (float)Math.Sin(angle) + tZ * (float)Math.Cos(angle) - tZ;
+
+                // Translate back vertex...
+                vertices[i].position.X += tX;
+                vertices[i].position.Y += tY;
+                vertices[i].position.Z += tZ;
             }
         }
 
@@ -72,6 +155,10 @@ namespace simple_3d_graphics_test
                     image.SetPixel(i, j, Color.White);
 
             Translate(cubeOne, new Vector3(trackX.Value, trackY.Value, trackZ.Value)); // -6, 8, -14, 857
+
+            if (trackRotX.Value != 0) RotateX(cubeOne, ((float)Math.PI / 180f) * trackRotX.Value);
+            if (trackRotY.Value != 0) RotateY(cubeOne, ((float)Math.PI / 180f) * trackRotY.Value);
+            if (trackRotZ.Value != 0) RotateZ(cubeOne, ((float)Math.PI / 180f) * trackRotZ.Value);
 
             /*trackX.Value = 0;
             trackY.Value = 0;
@@ -128,6 +215,21 @@ namespace simple_3d_graphics_test
         {
             labelDistance.Text = trackDistance.Value.ToString();
             distanceFromViewPlane = trackDistance.Value;
+        }
+
+        private void trackRotX_Scroll(object sender, EventArgs e)
+        {
+            labelRotX.Text = trackRotX.Value.ToString();
+        }
+
+        private void trackRotY_Scroll(object sender, EventArgs e)
+        {
+            labelRotY.Text = trackRotY.Value.ToString();
+        }
+
+        private void trackRotZ_Scroll(object sender, EventArgs e)
+        {
+            labelRotZ.Text = trackRotZ.Value.ToString();
         }
     }
 }
