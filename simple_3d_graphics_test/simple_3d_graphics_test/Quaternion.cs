018886b7a8885f417a3ab2d7ce9ef93b12de9a81
Simeon Laplev <thewiffedshot@gmail.com>
2018-03-25 17:11:07 +0300

    upload
    TODO: Implement world space rotations with quaternions instead of simple rotational matricies.

---
+++ simple_3d_graphics_test/simple_3d_graphics_test/Quaternion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace simple_3d_graphics_test
+{
+    class Quaternion
+    {
+        public Vector4 value { get; set; }
+
+        public Quaternion(Vector4 _value)
+        {
+            value = _value;
+        }
+    }
+}
